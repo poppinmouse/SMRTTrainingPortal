027670f2ca50e949b4cafd934008e48cf573af69
poppinmouse <1.kg.kywat@gmail.com>
2019-12-09 12:20:07 +0800

    Sent Email prepared.

---
+++ Assets/scripts/Classes/Email.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Email 
+{
+    public string interchangeAddress;
+    public string trainerAddress;
+    public string subject;
+    public string body;
+
+    public Email(string interchangeAddress, string trainerAddress, string subject, string body)
+    {
+        this.interchangeAddress = interchangeAddress;
+        this.trainerAddress = trainerAddress;
+        this.subject = subject;
+        this.body = body;
+    }
+}
--- Assets/scripts/PostController.cs
+++ Assets/scripts/PostController.cs
@@ -8,6 +8,7 @@ public class PostController : MonoBehaviour
 {
     public string url;
     public Dropdown trainingTypeDD;
+    public static PostController postControllerInstance;
 
     public InputField[] nameFields;
     public InputField[] iDFields;
@@ -16,12 +17,23 @@ public class PostController : MonoBehaviour
     [SerializeField]
     public Trainee[] trainees;
 
+    public Dropdown[] addressDDs;
+    public InputField subject;
+    public InputField body;
+
+    public Email email;
+    public GameObject EmailTemplate;
+
     private string route;
 
     private int numberOfTrainees;
 
-    private void Start()
+    private void Awake()
     {
+        postControllerInstance = this;
+    }
+    private void Start()
+    {        
         numberOfTrainees = nameFields.Length;
         trainees = new Trainee[numberOfTrainees];
     }
@@ -29,6 +41,8 @@ public class PostController : MonoBehaviour
     public void Submit()
     {
         StartCoroutine(Upload());
+        GenerateEmail();
+        EmailTemplate.SetActive(true);
         //locally disable the selected date for this session
         //CalendarController._calendarInstance.dateLookUp[CalendarController._calendarInstance.selectedDate.ToString()].GetComponent<Button>().interactable = false;
     }
@@ -97,6 +111,63 @@ public class PostController : MonoBehaviour
         else
         {
             Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+    void GenerateEmail()
+    {
+        string emailSubject;
+        string emailBody;
+        string trainingType = trainingTypeDD.options[trainingTypeDD.value].text;
+
+        emailSubject = string.Format("Booking for {0} Training", trainingType);
+
+        emailBody = string.Format("Hi Interchange Personal,\n\n " +
+            "Here are the Bus Captains that need to attend the {0} Training.\n\n " +
+            "{1}  {2}   {3}\n\n" +
+            "{4}  {5}   {6}\n\n" +
+            "{7}  {8}   {9}\n\n",
+            trainingType, 
+            nameFields[0].text, iDFields[0].text, interchangeFields[0].text,
+            nameFields[1].text, iDFields[1].text, interchangeFields[1].text,
+            nameFields[2].text, iDFields[2].text, interchangeFields[2].text,
+            nameFields[3].text, iDFields[3].text, interchangeFields[3].text);
+
+        subject.text = emailSubject;
+        body.text = emailBody;
+
+    }
+
+    IEnumerator SentEmail()
+    {
+        WWWForm form = new WWWForm();
+
+        Email email = new Email(addressDDs[0].options[addressDDs[0].value].text, addressDDs[1].options[addressDDs[1].value].text, subject.text, body.text);
+
+        string emailJson = JsonUtility.ToJson(email);
+        Debug.Log(emailJson);
+
+        form.AddField("Email", emailJson);
+
+        UnityWebRequest www = UnityWebRequest.Post(url + "/" + route, form);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SentEmail());
         }
     }
 }
